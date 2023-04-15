a5605350d11cd3b1e0aa986ad81fc0ee17b7a561
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-15 16:52:04 +0300

    updated  jsx

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,22 +1,15 @@
 import React from "react";
 
 export default function Box(props) {
-    console.log(props.handleClick)
-    const [status, setStatus] = React.useState(props.on)
+    
 
-        //  function handleClick() {
-        //     setStatus( (prevStatus) => {
-        //         return !prevStatus
-        //     })
-        //  }
-
-        const styles ={ backgroundColor: status ?  "gold" : "green"}
+        const styles ={ backgroundColor: props.on ?  "gold" : "green"}
     
     
 
     return (
         <div style={styles} className="box" onClick={() => {
-            props.handleClick.toggle(props.id)
+            props.handleClick(props.id)
         }}></div>
     )
 }
