1795fa3cf617ce90c3e9688ba25d422fc1083ca2
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-15 16:20:34 +0300

    updated  jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import React from 'react'
+
 import boxes from './boxes'
 import Box from './Box'
 import './App.css'
@@ -8,14 +10,12 @@ function App() {
   const [squares, setSquares] = React.useState(boxes)
 
   const squaresHtml = squares.map((square) => {
-    return {
-      
-    }
+    return <Box />
   })
 
   return (
     <div className="box-container">
-
+      {squaresHtml}
     </div>
   )
 }
